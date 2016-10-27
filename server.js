7dcafb98af3c0808886cbb80efd1dc0ef265f32d
sergejuss <sergejus.stankevicius@gmail.com>
2016-10-27 14:28:29 +0300

    clementine with auth working on localhost

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 
 var express = require('express'),
 	routes = require('./app/routes/index.js'),
-	mongoose = require('mongoose');
+	mongoose = require('mongoose'),
+	passport = require('passport'),
+    session = require('express-session');
 var app = express();
+require('./app/config/passport')(passport);
 
 mongoose.connect(process.env.Mongo_URI);
 
@@ -11,8 +14,18 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+app.use('/common', express.static(process.cwd() + '/app/common'));
 
-routes(app);
+app.use(session({
+    secret: 'secretClementine',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+routes(app, passport);
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));    
